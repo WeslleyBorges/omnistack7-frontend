aacb0c673917eafb18fc7070547a4f5558a33375
Weslley <weslleynfs@gmail.com>
2019-11-24 00:31:12 -0400

    Creating a new repo for this layer (frontend)

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Routes from './routes'
+import { BrowserRouter } from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+        <Routes></Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+ 
+import './Header.css'
+
+export default function Header() {
+  return <div></div>
+}
