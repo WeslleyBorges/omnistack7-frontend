ea48632eb263b6354654d6b192bb566785174615
Weslley <weslleynfs@gmail.com>
2019-11-24 18:35:03 -0400

    progress omnistack7

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import Routes from './routes'
 import { BrowserRouter } from 'react-router-dom'
+
+import Routes from './routes'
+import Header from './components/Header'
 
 function App() {
   return (
     <BrowserRouter>
-        <Routes></Routes>
+      <Header></Header>
+      <Routes></Routes>
     </BrowserRouter>
   );
 }
--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,8 +1,60 @@
 import React, { Component } from 'react'
 
+import './Feed.css'
+import more from '../assets/more.svg'
+import like from '../assets/like.svg'
+import comment from '../assets/comment.svg'
+import send from '../assets/send.svg'
+import api from '../services/api'
+
 class Feed extends Component {
+  state = {
+    feed: []
+  }
+
+  async componentDidMount() {
+    const response = await api.get('posts')
+    console.log(response.data)
+    this.setState({ feed: response.data })
+  }
+
+  renderPosts() {
+    return (
+      <section id="post-list">
+        { this.state.feed.map(post => (
+          <article key={post._id}>
+            <header>
+              <div className="user-info">
+                <span>{post.author}</span>
+                <span className="place">{post.place}</span>
+              </div>
+              <img src={more} alt="Mais"></img>
+            </header>
+
+            <img src={`http://localhost:5000/${post.image}`} alt="Xsde"></img>
+
+            <footer>
+              <div className="actions">
+                <img src={like} alt="Like"></img>
+                <img src={comment} alt="Comment"></img>
+                <img src={send} alt="Direct"></img>
+              </div>
+
+              <strong>{post.likes} curtidas</strong>
+
+              <p>
+                {post.description}
+                <span>{post.hashtags}</span>
+              </p>
+            </footer>
+          </article>
+        )) }
+      </section>
+    )
+  }
+
   render() {
-    return <h1>Feed</h1>
+    return this.renderPosts()
   }
 }
 
